3c32cb2b5e3b27a78c653d3af0d99d70f15d823e
Denise <yu.denise.d@gmail.com>
2014-12-16 10:45:58 +0000

    cleaning up syntax

--- public/thermocat.js
+++ public/thermocat.js
@@ -5,53 +5,56 @@ function Thermocat() {
   this.type = 'cat';
   this.minimumTemperature = 10;
   this.maximumTemperature = 25;
-};
+}
 
 Thermocat.prototype.increaseTemperature = function() {
-  return this.increaseTemperatureBy(1)
+  return this.increaseTemperatureBy(1);
 };
 
 Thermocat.prototype.increaseTemperatureBy = function(degrees) {
   if (this.temperature === this.maximumTemperature) {
-    return this.maximumTemperature
-  };
-  return this.temperature = this.temperature + degrees
-};
+    return this.maximumTemperature;
+  } else {
+    return this.temperature = this.temperature + degrees;
+  }
+}
 
 Thermocat.prototype.decreaseTemperature = function() {
-	return this.decreaseTemperatureBy(1)
-};
+	return this.decreaseTemperatureBy(1);
+}
 
 Thermocat.prototype.decreaseTemperatureBy = function(degrees) {
-  if (this.temperature === this.minimumTemperature) { 
-  	return this.minimumTemperature };
-  return this.temperature = this.temperature - degrees
-};
+  if (this.temperature === this.minimumTemperature) {
+  	return this.minimumTemperature;
+  } else {
+    return this.temperature = this.temperature - degrees;
+  }
+}
 
 Thermocat.prototype.resetTemperature = function() {
   return this.temperature = 20;
-};
+}
 
 Thermocat.prototype.changeColor = function () {
   if (this.temperature <= 18) {
-    this.color = 'green'
-  };
+    this.color = 'green';
+  }
   if (this.temperature >= 18 && this.temperature <= 25) {
-    this.color = 'yellow'
-  };
+    this.color = 'yellow';
+  }
   if (this.temperature >= 25) {
-  	this.color = 'red'
-  };
-};
+  	this.color = 'red';
+  }
+}
 
 Thermocat.prototype.turnOffPowerSave = function() {
   this.resetTemperature();
 	this.isPowerSaveMode = false;
 	this.maximumTemperature = 32;
-};
+}
 
 Thermocat.prototype.turnOnPowerSave = function() {
   this.resetTemperature();
   this.isPowerSaveMode = true;
   this.maximumTemperature = 25;
-};
+}
